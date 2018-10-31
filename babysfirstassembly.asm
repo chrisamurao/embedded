32b36113722186148c2df94dec93d4d08c63a175
Chris Amurao <freshpotatoes@gmail.com>
2018-10-31 01:46:44 -0700

    made file - used arithmetic for delay
    delayed blinky light for MSP-EXP430F5529LP using assembly, my first assembly code lol

---
+++ babysfirstassembly.asm
@@ -0,0 +1,65 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+			; Set GPIO P1.0 to be an output
+			BIS.B #1, &PADIR_L
+
+MainLoop: ; this must start in the first column
+
+			; Set output on GPIO P1.0 to high voltage
+			BIS.B #1, &PAOUT_L
+
+			MOV #80085, R4 ; works out to a delay of ~0.3secs
+
+DelayOn:
+			SUB #1, R4 ; subtract 1 from r4
+			CMP #0, R4 ; compare R4 to 0
+			JNZ DelayOn ; while R4 != 0, keep running DelayOn loop
+
+			; Set output on GPIO P1.0 to low voltage
+			BIC.B #1, &PAOUT_L
+
+			MOV #80085, R4
+
+DelayOff:
+			SUB #1, R4
+			CMP #0, R4
+			JNZ DelayOff
+
+                                            
+			JMP MainLoop
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
